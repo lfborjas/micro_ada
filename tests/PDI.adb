144fa63d4450ec843a652aa6faa11d6c58089194
lfborjas <lfborjas@hotmail.com>
2009-09-13 01:19:48 -0600

    creado el directorio para pruebas, con un par de programas sencillos

---
+++ tests/PDI.adb
@@ -0,0 +1,65 @@
+WITH numbers;
+WITH text_io;
+PROCEDURE pdi IS
+
+   -- Maximum length of input line
+   line_length : CONSTANT := 80;
+
+   -- Position of last character on input line
+   last        : natural;
+   -- Flag indicating (if true) that a prompt for a user command must be output
+   repeat      : Boolean := true;
+    --Input line entered by user
+   response    : string;
+
+BEGIN -- pdi
+
+   -- Determine input option from user
+   WHILE repeat LOOP
+
+      -- Assume no need to reprompt
+      repeat := false;
+
+      -- Prompt user and read response
+      text_io.new_line;
+      text_io.put_line 
+         ("Enter ""n"" for normal input from file ""input.dat""");
+      text_io.put_line 
+         ("   or ""r"" to restart from file ""checkpoint"": ");
+      text_io.get_line (item => response, last => last);
+
+      -- Interpret user response
+      IF (last = 1) THEN 
+         IF (response(1) = 'n') THEN
+
+            -- Take search parameters from "input.dat"
+            numbers.search.process_normal_input.start;
+
+         ELSIF (response(1) = 'r') THEN
+
+            -- Begin search from checkpoint file "pdi.ckp"
+            numbers.search.do_search.enter_start_search_from_checkpoint;
+
+         ELSE
+
+            -- Invalid user input (unrecognized character); must reprompt
+            repeat := true;
+            text_io.new_line;
+
+         END IF;
+
+      ELSE
+
+         -- Invalid user input (>1 character entered); must reprompt
+         repeat := true;
+         text_io.new_line;
+
+      END IF;
+
+   END LOOP;
+
+   --Begin monitoring keyboard for user request to enter commands
+   numbers.search.synchronize.start_monitor;
+
+END pdi;
+
---
+++ tests/programSmallMain.adb
@@ -0,0 +1,47 @@
+-- Main routine to start up "Small", a small text adventure game to
+-- demonstrate Ada 95.
+
+--
+-- Copyright (C) 1996 Ada Resource Association (ARA), Columbus, Ohio.
+-- Author: David A. Wheeler
+--
+
+-- For documentation see the following URL:
+--   http://www.adahome.com//Tutorials/Lovelace/small.htm
+
+with Text_IO, Ada.Strings.Unbounded, Ustrings, World;
+use  Text_IO, Ada.Strings.Unbounded, Ustrings;
+
+with Parser;
+
+procedure Small is
+  Command : Unbounded_String; -- Contains user's current command.
+  Quit    : Boolean := False;
+begin
+ Put_Line("Welcome to a Small World!");
+
+ World.Setup;
+
+ while not Quit loop
+  New_Line;
+  Put_Line("Your Command?");
+  Get_Line(Command);
+  Parser.Execute(Command, Quit);
+ end loop;
+
+ Put_Line("Bye!");
+end Small;
+
+--
+-- Permission to use, copy, modify, and distribute this software and its
+-- documentation for any purpose and without fee is hereby granted,
+-- provided that the above copyright and authorship notice appear in all
+-- copies and that both that copyright notice and this permission notice
+-- appear in supporting documentation.
+-- 
+-- The ARA makes no representations about the suitability of this software
+-- for any purpose.  It is provided "as is" without express
+-- or implied warranty.
+-- 
+
+
