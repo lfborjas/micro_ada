4a11749cf135c104d5bf31bfec95964ac77dea3d
lfborjas <lfborjas@hotmail.com>
2009-09-13 20:24:14 -0600

    probando errores en declarative part y con begins y ends faltantes

--- tests/programSmallMain.adb
+++ tests/programSmallMain.adb
@@ -13,7 +13,7 @@
 procedure Small is
   Command : Unbounded_String; -- Contains user's current command.
   Quit    : Boolean := False;
-begin
+--begin
  Put_Line("Welcome to a Small World!");
 
  World.Setup;
@@ -28,6 +28,23 @@ begin
  Put_Line("Bye!");
 end Small;
 
+procedure Small is
+  Command : Unbounded_String; -- Contains user's current command.
+  Quit    : Boolean := False;
+begin
+ Put_Line("Welcome to a Small World!");
+
+ World.Setup;
+
+ while not Quit loop
+  New_Line;
+  Put_Line("Your Command?");
+  Get_Line(Command);
+  Parser.Execute(Command, Quit);
+ end loop;
+
+ Put_Line("Bye!");
+end Small;
 --
 -- Permission to use, copy, modify, and distribute this software and its
 -- documentation for any purpose and without fee is hereby granted,
