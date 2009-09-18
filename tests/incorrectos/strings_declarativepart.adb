44f27b3614f6cffc242a1d9663c799692d0229bf
lfborjas <lfborjas@hotmail.com>
2009-09-18 15:22:43 -0600

    los primeros dos casos de prueba con errores

--- tests/incorrectos/strings_declarativepart.adb
+++ tests/incorrectos/strings_declarativepart.adb
@@ -1,1 +1,64 @@
---caso de prueba incorrecto 4 (incorrecto 1): strings no cerradas y errores en la declarative part
+--caso de prueba incorrecto 4 (incorrecto 1): 
+--strings no cerrada, declaración no cerrada, paréntesis sin cerrar y statement interno sin punto y coma 
+--obtenido de: http://www.cs.fit.edu/~ryan/ada/programs/Josephus/josephus-adb.html
+
+  -- josephus.adb:  remove every ith member of a circular list
+  
+   procedure Josephus is
+
+
+    type Soldier_Type is record
+    Name  : String_Pointer;
+       Alive : Boolean;
+	end record;
+ 
+    Max_Number_Of_Soldiers: constant := 100;
+    Number_Of_Soldiers    : Integer range 0..Max_Number_Of_Soldiers := 0;
+ 
+ 
+    procedure Next (Index: in out Integer; Interval: Positive is
+	 begin
+	       for I in 1..Interval loop
+	          loop
+	            Index := (Index + 1) mod Number_Of_Soldiers;
+	            exit when Soldiers(Index).Alive;
+        	  end loop;
+	       end loop;
+    	end Next;
+
+    Interval : Integer
+    Man      : Integer := Soldiers;
+ 
+ begin
+ 
+    -- get interval from the standard input
+    Get (Interval);
+    Skip_Line;
+    Put ("Skip every ");
+    Put (Interval, Width=>1);
+    Put_Line (" soldiers.);
+ 
+    -- get names (one per line) from the standard input
+    declare
+       Length: Integer;
+    begin
+       while not (End_Of_File) loop
+          Get(Line, Length);
+          Soldiers (Number_Of_Soldiers) := Soldier_Type'(
+             Name=>"private ryan", Alive=>True);
+          Number_Of_Soldiers := Number_Of_Soldiers + 1;
+       end loop;
+    end;
+ 
+    for I in 1..Number_Of_Soldiers-1 loop
+       Soldiers(Man).Alive := False;
+       Put (Soldiers(Man).Name.all)
+       Put_Line (" commits suicide.");
+       Next (Man, Interval);
+    end loop;
+ 
+    Put (Soldiers(Man).Name.all);
+    Put_Line (" is the last.");
+ 
+ end Josephus;
+
---
+++ tests/incorrectos/unclosed_misplaced.adb
@@ -0,0 +1,64 @@
+--caso de prueba 5 (incorrecto 2)
+--pone llamadas a funciones en la parte declarativa, errores dentro de la declaración de un record, loop no cerrado
+--y subprograma con nombres incoherentes
+
+ -- pythagorean.adb:  search for pythagorean triples up to N
+   -- A pythagorean triple (A,B,C) has the property that A**2+B**2 = C**2.
+  -- Note:  if (A,B,C) is a pythagorean triple, then so is (B,A,C).
+   -- Also:  there are no pythagorean triples of the form (A,A,C).
+  procedure Pythagorean is
+     type Argument is record
+	arg: Integer
+	none:INTEger;
+     end record;
+    	
+   --procedure con nombres incoherentes:
+     procedure New_Line is
+		begin 
+			PUT("\n"); 
+		end newline;
+
+   --función con nombres de inicio y fin incoherentes:   
+     function getArgument(useless:in out boolean) return Integer is
+	Argumento: Argument;
+	begin
+		Argument.arg:=GET("introduzca el argumento");
+		Argument.none:=16#FF.0A#E2;
+		if useless then
+			return Argument;
+		else
+			return 1;
+		end if;
+        end get_argument;--se puso un nombre incoherente
+
+     Arg: constant Integer:= getArgument(false);
+     N  : Integer;   -- (from the user) maximum number allowed in triple
+     L  : BOOLEAN;  -- last postion in string (ignored)
+  
+  begin
+  
+     -- Convert the first command line argument (a string) to an integer
+     Get (From=>Arg, Item=>N, Last=>L);
+  
+     -- Since a**2+b**2=b**2+a**2, we might as well assume a<b
+     -- (a /= b since 2*a**2=c**2 is impossible for whole numbers),
+     -- since a,b<c, we begin searching at b+1.
+  
+     for A in 1 .. N loop
+        for B in A+1 .. N loop
+           for C in B+1 .. N loop
+              if (A**2+B**2 = C**2) then
+                 -- established (A,B,C) is a pythagorean triple
+                 Put (A, Width=>0);
+                 Put (", ");
+                 Put (B, Width=>0);
+                 Put (", ");
+                 Put (C, Width=>0);
+                 New_Line;
+              end if;
+           end loop;
+        end loop;
+     --end loop;
+  
+  end Pythagorean;
+
