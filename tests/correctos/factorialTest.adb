ae4c76edfc5818b0f31024aaaded53b2142ee0a0
lfborjas <lfborjas@hotmail.com>
2009-09-18 12:31:55 -0600

    arreglada la indentación de los casos correctos de prueba

--- tests/correctos/factorialTest.adb
+++ tests/correctos/factorialTest.adb
@@ -3,31 +3,31 @@
 
 procedure Main is
 --parte declarativa
-Var1: Integer := "2_300";
-function Factorial(A : in Integer) return Integer is
-begin
- if A < 0 then                -- Stop recursion if A <= 0.
-	Put("Error numérico");
- elsif A = 0 then
-    return 1;
- else
-   return A * Factorial(A - 1);   -- recurse.
- end if;
-end Factorial;
-Var2: Float := "300_000.257_890";
-Var3: Boolean := TRue;
-Prompt: Integer;
+	Var1: Integer := "2_300";
+	function Factorial(A : in Integer) return Integer is
+		begin
+			 if A < 0 then                -- Stop recursion if A <= 0.
+		         Put("Error numérico");
+			 elsif A = 0 then
+			    return 1;
+			 else
+			   return A * Factorial(A - 1);   -- recurse.
+			 end if;
+		end Factorial;
+	Var2: Float := "300_000.257_890";
+	Var3: Boolean := TRue;
+	Prompt: Integer;
 --inicio de Main
 begin 
-Prompt:= get("¿Desea continuar?(1/0)");
-while Prompt != 0 loop
-	For i in 1..10 loop
-		if i mod 2 then 
-			put(Factorial(Var1));	
-		end if;
-	end loop;--end del for
-Prompt := get("¿Desea continuar (1/0)?");
-end loop; --end del while
+	Prompt:= get("¿Desea continuar?(1/0)");
+	while Prompt != 0 loop
+		For i in 1..10 loop
+			if i mod 2 then 
+				put(Factorial(Var1));	
+			end if;
+		end loop;--end del for
+	Prompt := get("¿Desea continuar (1/0)?");
+	end loop; --end del while
 end Main;
 
 
--- tests/correctos/records_loop.adb
+++ tests/correctos/records_loop.adb
@@ -2,39 +2,40 @@
 --sacado de: http://www.dwheeler.com/lovelace/s6s6.htm
 procedure Main is
 --declaramos el record:
-type Date is
-  record
-   Day   : Integer range 1 .. 31;
-   Month : Integer range 1 .. 12;
-   Year  : Integer range 1 .. 4000 := 1995;
-  end record;
+	type Date is
+	  record
+	   Day   : Integer range 1 .. 31;
+	   Month : Integer range 1 .. 12;
+	   Year  : Integer range 1 .. 4000 := 1995;
+	  end record;
 
 --declaramos otra variable:
-Prompt: Integer := 0;
+	Prompt: Integer := 0;
 --ahora, otro record
-type Complex is
-  record
-    Real_Part, Imaginary_Part : Float := 0.0;
-  end record;
+	type Complex is
+	  record
+	    Real_Part, Imaginary_Part : Float := 0.0;
+	  end record;
 --ahora un procedure  
 	procedure Demo_Date is  
  	   Ada_Birthday : Date;
-	 begin
-	   Ada_Birthday.Month := 12;
-	   Ada_Birthday.Day   := 10;
-	   Ada_Birthday.Year  := 1815;
-	 end Demo_Date;
---ahora, otra variable:
-ComplexPrompt: Complex;
-rPrompt: Float:=0.0;
-iPrompt: Float:=0.0;
+           begin
+	  	 Ada_Birthday.Month := 12;
+		   Ada_Birthday.Day   := 10;
+		   Ada_Birthday.Year  := 1815;
+	   end Demo_Date;
+--ahora, otras variables:
+	ComplexPrompt: Complex;
+	rPrompt: Float:=0.0;
+	iPrompt: Float:=0.0;
+--otra función:
 	function getPrompt(r:Float; i:Float) return Complex is
 		retVal: Complex;
-	begin						
-		retVal.Real_part:= get("escriba algún número...");  
-		iPrompt.Imaginary_Part:= get("escriba algún otro número");		
-		return retVal;
-	end;
+		begin						
+			retVal.Real_part:= get("escriba algún número...");  
+			iPrompt.Imaginary_Part:= get("escriba algún otro número");		
+			return retVal;
+		end;
 begin --Main
 	loop
 		ComplexPrompt:= getPrompt(rPrompt, iPrompt);
