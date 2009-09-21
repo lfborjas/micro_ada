c67bd79a8279150b33c0bce19fa6626ff15cfc08
lfborjas <lfborjas@hotmail.com>
2009-09-21 14:13:24 -0600

    un pequeño cambio a un programa correcto

--- tests/correctos/nested_procs.adb
+++ tests/correctos/nested_procs.adb
@@ -1,9 +1,10 @@
 --caso de prueba 2: procedimientos anidados, parámetros in out y out, goto.
 
 procedure lastex is
-	
+	X: float := 0.0;
+        Y: constant boolean :=(true and then false ) or else (true xor true);	
 	procedure p(n: in out Integer; useless: out boolean) is
-
+                type nullrecord is record null; end record;
 		procedure show is 
 		begin
 			 <<Sort>>			 
