de67422f46b4b8291ea36b63d12b5267ea6c4709
lfborjas <lfborjas@hotmail.com>
2009-10-15 13:19:07 -0600

    La clase de la tabla de símbolos ahora tiene un método bool para poner y siempre pone y busca keys en lower-case (porque ada es case insensitive). El método put devuelve false si ya había una key en la tabla igual a la que se manda

--- AdaSemantic/LinkedSymbolTable.java
+++ AdaSemantic/LinkedSymbolTable.java
@@ -13,7 +13,13 @@ public class LinkedSymbolTable{
 	private LinkedSymbolTable ancestor;
 	
 	public LinkedSymbolTable(LinkedSymbolTable ancestor){
+		table=new HashMap<String, Type>();
 		this.ancestor= ancestor;
+	}
+	
+	public LinkedSymbolTable(){
+		table=new HashMap<String, Type>();
+		this.ancestor=null;	
 	}
 	
 	public LinkedSymbolTable getAncestor(){
@@ -24,8 +30,15 @@ public class LinkedSymbolTable{
 		return this.table;
 	}	
 
-	public void put(String id, Type tipo){
-		table.put(id, tipo);
+	public boolean put(String id, Type tipo){
+		//because of the case insensitiveness of ada:
+		id=id.toLowerCase();
+		if(!(this.table.containsKey(id))){
+			table.put(id, tipo);
+			return true;
+		}else{//shouldn't let it overwrite the existing key!
+			return false;
+		}
 	}
 	
 	/**Searches iteratively up the linked list of scopes the required identifier 
@@ -33,6 +46,8 @@ public class LinkedSymbolTable{
 	   @param id the id to search for	
 	*/
 	public Type get(String id){
+		//because of ada's case insensitiveness:
+		id=id.toLowerCase();
 		for(LinkedSymbolTable t=this; t != null; t= t.getAncestor()){
 			Type found=t.getTable().get(id);
 			if(found != null)
