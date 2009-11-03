b1eb4709e81642111e842f0a3f44fcbb745a64b7
lfborjas <lfborjas@hotmail.com>
2009-11-02 23:17:14 -0600

    las nuevas clases y cosas en la GUI

---
+++ ADA95_Compilador_10611066/src/AdaSemantic/FunctionType.java
@@ -0,0 +1,96 @@
+package AdaSemantic;
+import java.util.ArrayList;
+
+public class FunctionType extends Type{
+	/**a.k.a. the return type of the function*/
+	private Type range;
+	/*The domain of the function is the product of this Type*/
+	public FunctionType(){
+		this.range=null;
+		this.width=0;
+		this.name="function";
+	}
+		
+	public FunctionType(Type range){
+		super();
+		this.range=range;
+		this.width=range.width;
+		this.name="function";
+	}
+
+	public FunctionType(Type range, ArrayList<Type> domain){
+		this.product=domain;
+		this.range=range;
+		this.width=range.width;
+		this.name="function";
+	}
+
+	public FunctionType(ArrayList<Type> domain){
+		this.product=domain;
+		this.width=0;
+		this.range=null;//Should I create a void class?
+		this.name="function";
+	}
+
+	
+	public Type getRange(){
+		return this.range;
+	}
+
+	public void setRange(Type r){
+		range=r;
+	}
+
+	public boolean isPrimitive(){
+		return false;
+	}
+
+	public String toString(){
+		StringBuilder retVal=new StringBuilder();		
+		if (this.range==null)
+			retVal.append("Void ");
+		else
+			retVal.append(this.range.toString());
+		retVal.append(" function with (");
+		if (this.product.isEmpty()){
+			retVal.append(" with no parameters.");
+			return retVal.toString();
+		}
+
+		for(int i=0;i<this.product.size()-1;i++)
+			retVal.append(this.product.get(i)+ ", ");
+		retVal.append(this.product.get(product.size()-1)+ " )");
+		return retVal.toString();			
+	}
+
+        public boolean equals(Object o){
+                if(o==null)
+                        return false;
+                if(!(o instanceof FunctionType))
+                        return false;
+                if(o==this)
+                        return true;
+                FunctionType temp=(FunctionType) o;
+                //must have the same width!
+                if (temp.getWidth() != this.width)
+                        return false;
+                //if it does, then must have the same return type:
+		if (!(temp.getRange().equals(this.range)))
+			return false;
+		//if it has the same return value, must have the same number of parameters
+                if(temp.getProduct().size() != this.product.size())
+                        return false;
+                //if so, then the parameters must be equal in type and order:
+                ArrayList<Type> tempProduct=temp.getProduct();
+                for(int i=0;i<this.product.size();i++)
+                        if(!(tempProduct.get(i).equals(this.product.get(i))))
+                                return false;
+                return true;
+        }
+	
+	public boolean isNumeric(){
+		return false;
+	}
+
+		
+}
---
+++ ADA95_Compilador_10611066/src/AdaSemantic/LinkedSymbolTable.java
@@ -0,0 +1,90 @@
+package AdaSemantic;
+import java.util.HashMap;
+/**
+	A class to represent a symbol table for a scope
+	@author Luis Felipe Borjas
+	@date 09 Oct 09
+*/
+
+public class LinkedSymbolTable{
+	/**The Symbol table for this scope*/
+	private HashMap<String, AdaSymbol> table;
+	/**The inmedeate outer scope*/
+	private LinkedSymbolTable ancestor;
+	
+	public LinkedSymbolTable(LinkedSymbolTable ancestor){
+		table=new HashMap<String, AdaSymbol>();
+		this.ancestor= ancestor;
+	}
+	
+	public LinkedSymbolTable(){
+		table=new HashMap<String, AdaSymbol>();
+		this.ancestor=null;	
+	}
+	
+	public LinkedSymbolTable getAncestor(){
+		return this.ancestor;
+	}
+	
+	public HashMap<String, AdaSymbol> getTable(){
+		return this.table;
+	}	
+
+	public boolean put(Object oid, AdaSymbol tipo){
+		String id=(String)oid;
+		//because of the case insensitiveness of ada:
+		id=id.toLowerCase();
+		if(!(this.table.containsKey(id))){
+			table.put(id, tipo);
+			return true;
+		}else{//shouldn't let it overwrite the existing key!
+			return false;
+		}
+	}
+	
+	/**Searches iteratively up the linked list of scopes the required identifier 
+	   cf. The book by Aho, Lam; pp 88-89.	
+	   @param id the id to search for	
+	*/
+	public AdaSymbol get(Object oid){
+		String id=(String)oid;
+		//because of ada's case insensitiveness:
+		id=id.toLowerCase();
+		AdaSymbol found=new AdaSymbol();
+		//for records:
+		String [] splitId=id.split("\\.");
+		for(LinkedSymbolTable t=this; t != null; t= t.getAncestor()){
+			found=t.getTable().get(splitId[0]);
+			if(found != null){
+				if(splitId.length == 1)//is not a selected component query
+					return found;
+				else//it IS a selected component query
+					break;
+			}
+		}
+		//the old one: if it's NOT a selected component query NOR was the id found:
+		if (found == null)	
+			return null;
+		//as it is a selected component query, then the type of this found object MUST be selectable (i.e. Record)			
+		if(!(found.type instanceof RecordType))
+			return null;					
+		//it is a record, so, try to fetch it's next components (>splitId[1])		
+		RecordType f;
+		for(int i=1; i<splitId.length-1; i++){
+			f=(RecordType)found.type;		
+			found=f.symbolTable.getTable().get(splitId[i]);
+			if(found != null){
+				if(!(found.type instanceof RecordType))
+					return null;				
+			}else{
+				return null;
+			}
+			
+		}
+		//got to the end, try to fetch the last one (which may be null):
+		f=(RecordType)found.type;
+		found=f.symbolTable.getTable().get(splitId[splitId.length-1]);
+		return found;
+	}	
+	
+}
