a0519238f4def3fcd9697a2cb9f6b5f6df853953
lfborjas <lfborjas@hotmail.com>
2009-11-26 02:39:42 -0600

    primer intento de backpatching de if...

--- AdaSemantic/ListaSalto.java
+++ AdaSemantic/ListaSalto.java
@@ -3,14 +3,19 @@ import java.util.ArrayList;
 /**Representa una lista de saltos para backtracking.*/ 
 public class ListaSalto{
 	public ArrayList<Integer> lista;
-
+	public ListaSalto(){
+		this.lista=new ArrayList<Integer>();
+	}
 	public ListaSalto(int index){
 		lista=new ArrayList<Integer>();
 		lista.add(new Integer(index));
 	}
 	/**Copia los valores de la otra lista a la presente. Idealmente, nada debería sobreescrirse!*/
-	public void fusiona(ListaSalto other){
-		this.lista.addAll(other.lista);
+	public static ListaSalto fusiona(ListaSalto another, ListaSalto other){
+		ListaSalto rv=new ListaSalto();
+		rv.lista.addAll(another.lista);		
+		rv.lista.addAll(other.lista);
+		return rv;
 	}
 		
 }
