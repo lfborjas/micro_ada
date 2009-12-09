1aa55ec4a752501ba21c1fc0288b84a14b7c6d3d
lfborjas <lfborjas@hotmail.com>
2009-12-08 20:00:45 -0600

    agregadas las pruebas de código intermedio y objeto

---
+++ tests/codeTests/if_statements.adb
@@ -0,0 +1,23 @@
+procedure Main is
+a: integer:= 1;
+begin
+	if a > 10 then
+		put("mayor que diez");
+	end if;
+
+	if a < 9 then
+		put("menor que nueve");
+	else
+		null;
+	end if;
+
+	if a=0 then
+		a:= 1;
+	elsif a=1 then
+		a:=2;
+	elsif a=2 then
+		a:=3;
+	else
+		a:=4;
+	end if;
+end Main;
