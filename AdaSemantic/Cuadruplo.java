c46e4d3bdc910ce5c4af81ca509b2cd297907100
lfborjas <lfborjas@hotmail.com>
2009-12-08 20:00:11 -0600

    mejorado el toString de cuadruplo

--- AdaSemantic/Cuadruplo.java
+++ AdaSemantic/Cuadruplo.java
@@ -35,12 +35,18 @@ public class Cuadruplo{
 		this.res=res;
 	}	
 	public String  toString(){
-		String r=(!res.isEmpty()) ? res+"=" : "";
+		String r=(!res.isEmpty()) ? res+":=" : "";
 		String a1=(!arg1.isEmpty()) ? arg1+" " : "";
 		String o= (!operador.isEmpty()) ? operador+ " " : "";
 		/*Para que el if se mire pinta: */
 		if(operador.contains("if"))
-			return operador+" "+arg1+" "+ arg2+" goto "+res;
+			return operador.split("_")[0]+" "+arg1+" "+operador.split("_")[1]+" "+ arg2+" goto "+res;
+		else if(operador.contains(":="))
+			return res+" := "+ arg1;
+		else if(operador.equals("goto"))
+			return "goto "+res;
+		else if(operador.matches("put|get|call|param|glbl|function|return|initFunction"))
+			return operador+" "+ arg1+" "+arg2;
 		return r+a1+o+arg2;
 	}
 		
