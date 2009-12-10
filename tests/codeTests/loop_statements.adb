89c3ebc198836d4c0d739988ae6179af7cbc6fd7
lfborjas <lfborjas@hotmail.com>
2009-12-10 12:05:12 -0600

    listos los for (normal e in reverse)

--- tests/codeTests/loop_statements.adb
+++ tests/codeTests/loop_statements.adb
@@ -9,4 +9,22 @@ begin
 			a:=a-2;
 		end if;
 	end loop;
+
+	for i in reverse 1..10
+	loop
+		if a mod 3 = 0 then
+			a:=a+3;
+		else
+			a:=a-3;
+		end if;
+	end loop;
+
+	loop
+		if a<0 then
+			exit;
+		end if;
+		put("hola mundo");
+		a:=a+1;
+	exit when a>10;
+	end loop;
 end Main;
