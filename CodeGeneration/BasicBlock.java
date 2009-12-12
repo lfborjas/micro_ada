6dc036058ef40bcc49b53d4026d7247f7cbbffbc
lfborjas <lfborjas@hotmail.com>
2009-12-11 21:29:38 -0600

    creación de la clase de los bloques básicos

---
+++ CodeGeneration/BasicBlock.java
@@ -0,0 +1,24 @@
+package CodeGeneration;
+/**Para representar los bloques básicos.*/
+class BasicBlock{
+	public String label;
+	public int beginning;
+	public int end;
+	/**El prefijo por defecto para las etiquetas*/
+	public static final String defaultPrefix="L";
+	public BasicBlock(){
+		this.label="";
+		this.beginning=0;
+		this.end=0;
+	}
+
+	public BasicBlock(String l, int b, int e){
+		this.label=l;
+		this.beginning=b;
+		this.end=e;
+	}
+
+	public String toString(){
+		return String.format("%s: [%d - %d]", this.label, this.beginning, this.end);
+	}
+}
